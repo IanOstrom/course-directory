c4a13f9671cd3fdccc4d0a7fdf655dba0067d320
IanOstrom <ianrostrom@gmail.com>
2020-04-22 15:36:10 -0400

    Updates

---
+++ precache-manifest.f2dbd6b000ed19b460edd408259ebf95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de953ac7691e42f2dee065c2fa6b472f",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "3ab7eea11d21ccf9defe",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "48bd90c7704739286562",
+    "url": "/course-directory/static/js/2.764d3bdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/course-directory/static/js/2.764d3bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab7eea11d21ccf9defe",
+    "url": "/course-directory/static/js/main.564bff62.chunk.js"
+  },
+  {
+    "revision": "de93b7aa1e262e5adaad",
+    "url": "/course-directory/static/js/runtime-main.2d6d2bf2.js"
+  }
+]);
